7cf5e49ae2beb18f079b07f2d1ea1cc33af8691c
haoxingjun <haoxingjun@bytedance.com>
2025-12-15 21:23:28 +0800

    modify model name in agent.py

--- 02-use-cases/data_analysis_with_datalake/agent.py
+++ 02-use-cases/data_analysis_with_datalake/agent.py
@@ -1,3 +1,12 @@
+'''
+Author: haoxingjun
+Date: 2025-12-15 21:21:14
+Email: haoxingjun@bytedance.com
+LastEditors: haoxingjun
+LastEditTime: 2025-12-15 21:21:46
+Description: file information
+Company: ByteDance
+'''
 # 导入所有必要的模块
 import os
 import sys
@@ -52,7 +61,7 @@ tools = [
 
 # 创建带记忆的 Agent
 model_name = os.getenv(
-    "MODEL_AGENT_NAME", "doubao-seed-1-6-251015"
+    "MODEL_AGENT_NAME", "deepseek-v3-1-terminus"
 )  # 默认使用更主流的豆包模型
 root_agent = Agent(
     description="基于LanceDB的数据检索Agent，支持结构化和向量查询。典型问题包括：1.你有哪些数据？2.给我一些样例数据？3.Ang Lee 评分超过7分的有哪些电影？4.Ang Lee 评分超过7分的电影中，有哪个电影海报中含有动物？5.Life of Pi 的电影海报，变成视频。 返回显示电影海报为![老虎](https://example.com/image1.png), 返回视频并显示成<video src='https://example.com/video1.mp4' width='640' controls>分镜视频1</video>",
